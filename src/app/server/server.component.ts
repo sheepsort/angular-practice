5167ca6cb7685791f4bbaa718c4f1aad5a600f2a
JG930731 <JG930731@RDI-JGRIFFITH>
2019-09-09 11:34:46 -0500

    Property Bindings

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
-    serverId : number = 10;
-    serverStatus : string = 'offline';
+    serverId = 10;
+    serverStatus = 'offline';
 
     getServerStatus() {
         return this.serverStatus;
