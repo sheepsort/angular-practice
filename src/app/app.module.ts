12b4bbe4e775a31c796108dd55fb58b4ee83a5ff
JG930731 <JG930731@RDI-JGRIFFITH>
2019-09-10 16:20:54 -0500

    second assignment init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { AssignmentComponent } from './assignment/assignment.component';
+import { GameControlComponent } from './assignment/assignment_two/game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    AssignmentComponent
+    AssignmentComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
