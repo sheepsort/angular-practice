27d947974e37526326bf267d1d4f04971154ed61
JG930731 <JG930731@RDI-JGRIFFITH>
2019-09-10 16:46:15 -0500

    completed assignment 2, passing component data

--- src/app/assignment/assignment_two/game-control/game-control.component.ts
+++ src/app/assignment/assignment_two/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber: number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  startGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+  }
+
+  endGame() {
+    clearInterval(this.interval);
+  }
 }
